18add7e8406f81125ac4448d10810daa27b08f9e
Jan Blasko <honza@lucien144.net>
2020-11-24 10:36:23 +0100

    closes #5

--- src/features/hashtags.ts
+++ src/features/hashtags.ts
@@ -1,3 +1,5 @@
+import './hashtags.scss';
+
 export default (() => {
     const filterInput = document.querySelector('input[name=filter_text]') as HTMLInputElement;
     const filterBtn = document.querySelector('button#topic_search') as HTMLButtonElement;
@@ -7,13 +9,19 @@ export default (() => {
     }
 
     document.querySelectorAll('.wci').forEach((element) => {
-        element.innerHTML = element.innerHTML.replace(/(#)([A-Za-z0-9_]+)/g, `<a href="$&" class="fyx__tag">$&</a>`);
+        // https://jex.im/regulex/#!flags=&re=(%3F!%5B%5E%3C%5D*%3E)(%23%5Ba-z%5D%5B%5Cw-%5D%2B)
+        element.innerHTML = element.innerHTML.replace(/(?![^<]*>)(#[a-z][\w-]+)/gmi, `<span class="fyx__tag like-link">$&</span>`);
     });
 
     document.querySelectorAll('.fyx__tag').forEach((element) => {
         element.addEventListener('click', (event) => {
-            event.preventDefault();
-            filterInput.value = element.getAttribute('href') ?? '';
+            const currentTarget  = event.currentTarget as HTMLSpanElement;
+            if (currentTarget.parentElement instanceof HTMLAnchorElement) {
+                currentTarget.parentElement.click();
+                return;
+            }
+
+            filterInput.value = element.innerHTML ?? '';
             filterBtn.click();
         })
     });
